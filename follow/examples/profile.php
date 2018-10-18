f07f1e067f844037dacc5d00564935e1a8ad9f40
noah <noahcwoods@gmail.com>
2018-10-18 10:48:44 -0400

    ...

--- follow/examples/profile.php
+++ follow/examples/profile.php
@@ -71,10 +71,10 @@ if (!isset($_SESSION)) {
             <div class="container">
                 <div class="owner">
                     <div class="avatar">
-                        <img src="../assets/img/faces/joe-gardner-2.jpg" alt="Circle Image" class="img-circle img-no-padding img-responsive">
+                        <img src="<?php echo $_SESSION['img_url'];?>" class="img-circle img-no-padding img-responsive">
                     </div>
                     <div class="name">
-                        <h4 class="title">PURE IMAGINATION<br /></h4>
+                        <h4 class="title"><?php echo $_SESSION['firstname'] $_SESSION['lastname'];?><br /></h4>
 						<h6 class="description">Music Producer</h6>
                     </div>
                 </div>
