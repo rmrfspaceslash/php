db67d84bd154c465d3deef57212f33efcf40f751
noah <noahcwoods@gmail.com>
2018-10-23 10:33:47 -0400

    ...

--- follow/examples/edituser.php
+++ follow/examples/edituser.php
@@ -93,19 +93,19 @@ if (!isset($_SESSION)) {
 																			<form class="" action="index.html" method="post">
 																				<li>
 																					<p class="text-dark">Edit your firstname:</p>
-																					<input type="text" name="edit_firstname" placeholder="<?php echo $_SESSION['firstname'] ?>">
+																					<input type="text" name="edit_firstname" placeholder="<?php echo $_SESSION['firstname'] ?>"> <br/>
 																				</li>
 																				<li>
 																					<p class="text-dark">Edit your lastname:</p>
-																					<input type="text" name="edit_lastname" placeholder="<?php echo $_SESSION['lastname'] ?>">
+																					<input type="text" name="edit_lastname" placeholder="<?php echo $_SESSION['lastname'] ?>"> <br/>
 																				</li>
 																				<li>
 																					<p class="text-dark">Edit your title:</p>
-																					<input type="text" name="edit_title" placeholder="<?php echo $_SESSION['title'] ?>">
+																					<input type="text" name="edit_title" placeholder="<?php echo $_SESSION['title'] ?>"> <br/>
 																				</li>
 																				<li>
 																					<p class="text=dark">Edit your description:</p>
-																					<input type="text" name="edit_description" placeholder="<?php echo $_SESSION['description'] ?>">
+																					<input type="text" name="edit_description" placeholder="<?php echo $_SESSION['description'] ?>"> <br/>
 																				</li>
 																			</form>
                                 </ul>
