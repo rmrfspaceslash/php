fe2607f0d2d31b359c9b844ba50dcb1b9ccce767
noah <noahcwoods@gmail.com>
2018-10-23 10:49:51 -0400

    ...

--- follow/examples/edituser.php
+++ follow/examples/edituser.php
@@ -93,22 +93,22 @@ if (!isset($_SESSION)) {
 																			<form class="" action="index.html" method="post">
 																				<li>
 																					<p class="text-dark">Edit your firstname:</p>
-																					<input type="text" name="edit_firstname" placeholder="<?php echo $_SESSION['firstname']; ?>"> <br/>
+																					<input class="form-control" type="text" name="edit_firstname" placeholder="<?php echo $_SESSION['firstname']; ?>"> <br/>
 																				</li>
 																				<li>
 																				<Br/>
 																					<p class="text-dark">Edit your lastname:</p>
-																					<input type="text" name="edit_lastname" placeholder="<?php echo $_SESSION['lastname']; ?>"> <br/>
+																					<input class="form-control" type="text" name="edit_lastname" placeholder="<?php echo $_SESSION['lastname']; ?>"> <br/>
 																				</li>
 																				<li>
 																				<br/>
 																					<p class="text-dark">Edit your title:</p>
-																					<input type="text" name="edit_title" placeholder="<?php echo $_SESSION['title']; ?>"> <br/>
+																					<input class="form-control" type="text" name="edit_title" placeholder="<?php echo $_SESSION['title']; ?>"> <br/>
 																				</li>
 																				<li>
 																				<br/>
 																					<p class="text-dark">Edit your description:<p/>
-																					<textarea rows="4" cols="50" name="description" placeholder="<?php echo $_SESSION['description']; ?>"></textarea>
+																					<textarea class="form-control" rows="4" cols="50" name="description" placeholder="<?php echo $_SESSION['description']; ?>"></textarea>
 																				</li>
 																			</form>
                                 </ul>
