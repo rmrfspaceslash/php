41b401a7aa4e52b30b25d04ce766ec22e00a5cd4
noah <noahcwoods@gmail.com>
2018-10-29 23:34:51 -0400

    ...

--- follow/examples/users.php
+++ follow/examples/users.php
@@ -95,7 +95,7 @@ require('db.php');
 																				$sql2 = "SELECT userid, followid FROM fm_follows WHERE userid = '$userid'";
 																				$results = $conn->query($sql2);
 																				while ($rowdb2 = $results->fetch_assoc()) {
-																					if ($rowdb2['followid'] == $rowdb['followid']) {
+																					if ($_SESSION['userid'] == $rowdb2['userid'] && $rowdb2['followid'] == $rowdb['followid']) {
 																			 ?>checked><?php }else {?>><?php } } ?>
 																			<span class="form-check-sign"></span>
 																	</label>
