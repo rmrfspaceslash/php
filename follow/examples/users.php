cc5ba9614130a16e96cb0aab1f8160a16edca1fe
noah <noahcwoods@gmail.com>
2018-10-31 11:06:25 -0400

    ...

--- follow/examples/users.php
+++ follow/examples/users.php
@@ -110,13 +110,13 @@ while ($rowdb3 = $results2->fetch_assoc()) {
 																		<div class="col-md-3 col-sm-2  ml-auto mr-auto">
 								<div class="form-check">
 																	<label class="form-check-label">
-																			<input class="form-check-input" name="<?php echo $rowdb['firstname']; ?>" type="checkbox" value="Yes" <?php
+																			<input name="<?php echo $rowdb['firstname']; ?>" type="checkbox" value="Yes" <?php
 																				$userid = $_SESSION['userid'];
 																				$sql2 = "SELECT userid, followid FROM fm_follows WHERE userid = '$userid'";
 																				$results = $conn->query($sql2);
 																				while ($rowdb2 = $results->fetch_assoc()) {
 																					if ($_SESSION['userid'] == $rowdb2['userid'] && $rowdb2['followid'] == $rowdb['followid']) { $marker = true;
-																			 ?>checked><?php } } if ($marker == false) { ?> > <?php } ?>
+																			 ?>checked class="form-check-input bg-success"><?php } } if ($marker == false) { ?> class="form-check-input"> <?php } ?>
 																			<span class="form-check-sign"></span>
 																	</label>
 															</div>
