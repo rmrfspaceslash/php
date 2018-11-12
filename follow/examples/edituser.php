a5d0c6feb55f589f3aa46c4fab215672b177f1ca
noah <noahcwoods@gmail.com>
2018-11-12 11:33:51 -0500

    ...

--- follow/examples/edituser.php
+++ follow/examples/edituser.php
@@ -114,7 +114,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	}
 
 	if (isset($_POST['upload'])) {
-	$sql = "UPDATE fm_users SET img_url = '$target_file' WHERE users='$email'";
+	$sql = "UPDATE fm_users SET img_url = '$target_file' WHERE userid='$userid'";
 	$conn->query($sql);
 	}
 }
