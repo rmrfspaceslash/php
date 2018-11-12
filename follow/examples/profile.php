9388128b23b373a8b50d77a98dfcdb623189db17
noah <noahcwoods@gmail.com>
2018-11-12 12:36:26 -0500

    ...

--- follow/examples/profile.php
+++ follow/examples/profile.php
@@ -89,7 +89,7 @@ require('db.php');
                     <div class="col-md-6 ml-auto mr-auto text-center">
                         <p><?php echo $_SESSION['description']; ?></p>
                         <br/>
-                        <input class="btn btn-outline-default btn-round" href="edituser.php"><i class="fa fa-cog" href="edituser.php"></i> Settings</input>
+                        <input type="submit" value="Settings" class="btn btn-outline-default btn-round" href="edituser.php"></input>
                     </div>
                 </div>
                 <br/>
